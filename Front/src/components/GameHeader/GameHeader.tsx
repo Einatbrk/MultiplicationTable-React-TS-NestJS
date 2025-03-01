b547c585d433141eabcdc2fb5e35902914fbd5f1
Einatbrk <einat.barak3@gmail.com>
2025-03-01 20:23:41 +0200

    feat: Update image assets and styles; replace JPEGs with SVGs, enhance layout and responsiveness in game components

--- Front/src/components/GameHeader/GameHeader.tsx
+++ Front/src/components/GameHeader/GameHeader.tsx
@@ -1,11 +1,11 @@
 import '../../styles/index.scss'
 import { GameHeaderProps } from './GameHeader.types';
 
-const GameHeader: React.FC<GameHeaderProps> = ({ playerName, score }) => {
+const GameHeader: React.FC<GameHeaderProps> = ({ playerName }) => {
   return (
     <div className="game-header">
       <h1>
-        בהצלחה, {playerName}! <span className="score">ציון: {score}</span>
+        בהצלחה, {playerName}! 
       </h1>
     </div>
   );
